c80cf82f80088fff56f5864ed2409e40119adb38
Prudhvi Raj <raaviprudhviraj@gmail.com>
2024-03-23 01:53:06 -0400

    clean up files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent
-  ],
+  declarations: [],
   imports: [
     BrowserModule,
     AppRoutingModule,
