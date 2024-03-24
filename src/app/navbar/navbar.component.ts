74d3603486018cf2153fa551329ee6a2bb3a2e3b
Prudhvi Raj <raaviprudhviraj@gmail.com>
2024-03-24 13:50:14 -0400

    made the navbar responsive

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -8,8 +8,23 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-
+  // constructor to inject services
+  // variable to get element by id hammenu
+  @ViewChild('hammenu') hammenu!: ElementRef;
+  @ViewChild('closemenu') closemenu!: ElementRef;
+  @ViewChild('mobilemenu') mobilemenu!: ElementRef;
+  isOpen = true;
   constructor() { }
-
+  ngAfterViewInit() {
+    this.open(); // Access native elements here
+  }
+  // Open method to toggle navbar on click of hamburger icon in single method
+  open() {
+    this.isOpen = !this.isOpen; // Toggle state
+  
+    this.hammenu.nativeElement.classList.toggle('hidden');
+    this.closemenu.nativeElement.classList.toggle('hidden');
+    this.mobilemenu.nativeElement.classList.toggle('hidden');
+  }
 
 }
